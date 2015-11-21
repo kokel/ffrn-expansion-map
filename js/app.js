61c2bef98fac1ed9e47958c242e5de90186dfa17
brokenbike <ffms-nienberge-1@posteo.org>
2015-11-21 12:56:40 +0100

    Link zur Lizenz lieber https:// als http://
    Besser das sicherere Hypertext-Übertragungsprotokoll nutzen, auch wenn dafür der Link länger wird.

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 // init leaflet map
 var map = L.map('map').setView([49.47704787438876, 8.5638427734375], 10);
 L.tileLayer('http://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors <br/> Map tiles by CartoDB, under CC BY 3.0. Data by OpenStreetMap, under ODbL.'
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors <br/> Map tiles by CartoDB, under CC BY 3.0. Data by OpenStreetMap, under ODbL.'
 }).addTo(map);
 
 var geojson;
